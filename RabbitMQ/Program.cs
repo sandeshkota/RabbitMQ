503ef484801d54373177ccab28beb6e672af7ce1
Sandesh <Sandesh.Kota@socgen.com>
2018-03-20 20:02:03 +0530

    Basic Structure

--- RabbitMQ/Program.cs
+++ RabbitMQ/Program.cs
@@ -1,100 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
-using Contract;
 
-namespace Client
+namespace RabbitMQ
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting RabbitMQ Message Sender");
-            Console.WriteLine();
+            var rabbitMq = new RabbitMQ();
+            rabbitMq.SetUp();
 
-            var messageCount = 1;
-            var sender = new RabbitSender();
+            var type = args[0];
+            type = "1";
 
-            Console.WriteLine("Press enter key to send a message");
-            Console.WriteLine("Select the number for the Format and press Enter");
-            Console.WriteLine("1 = JSON");
-            Console.WriteLine("2 = XML");
-            Console.WriteLine("3 = Binary");
-            Console.WriteLine();
-
-            while (true)
+            switch (type)
             {
-                var number = Console.ReadLine();
-
-                if (number.ToUpper() == "Q") break;
-
-                var format = number;
-                if (format != "1" && format != "2" && format != "3") continue;
-
-                var messageObj = new Message { MyMessage = $"Message: {messageCount}" };
-
-                var messageBuffer = SerializeMessage(messageObj, format);
-
-                Console.WriteLine($"Foramt - {format}, Sending - {messageObj}");
-                sender.Send(messageBuffer, GetContentType(format));
-                messageCount++;
-            }
-
-            Console.ReadLine();
-        }
-
-
-        private static string GetContentType(string format)
-        {
-            switch (format)
-            {
+                case "OneWayMessaging":
                 case "1":
-                case "JSON":
-                    return "application/json";
+                    rabbitMq.CreateSetupForOneWayMessaging();
+                    break;
+                case "WorkerQueue":
                 case "2":
-                case "XML":
-                    return "text/xml";
+                    rabbitMq.CreateSetupForWorkerQueues();
+                    break;
+                case "PublishSubscribe":
                 case "3":
-                case "Binary":
-                    return "application/octet-stream";
+                    rabbitMq.CreateSetupForPublishSubscribe();
+                    break;
+                case "RPC":
+                case "4":
+                    rabbitMq.CreateSetupForRemoteProcedureCall();
+                    break;
+                case "Routing":
+                case "A1":
+                    rabbitMq.CreateSetupForRouting();
+                    break;
+                case "Topics":
+                case "A2":
+                    rabbitMq.CreateSetupForTopics();
+                    break;
+                case "Headers":
+                case "A3":
+                    rabbitMq.CreateSetupForHeaders();
+                    break;
+                case "ScatterGather":
+                case "A4":
+                    rabbitMq.CreateSetupForScatterGather();
+                    break;
+                case "Serialization":
+                case "S":
+                    rabbitMq.CreateSetupForSerialization();
+                    break;
+                case "MessageType":
+                case "M":
+                    rabbitMq.CreateSetupForMessageType();
+                    break;
+                case "LargeBufferedMessage":
+                case "LB":
+                    rabbitMq.CreateSetupForLargeBufferedMessage();
+                    break;
+                case "LargeChunkedMessage":
+                case "LC":
+                    rabbitMq.CreateSetupForLargeChunkedMessage();
+                    break;
+                case "BasicRetry":
+                case "BR":
+                    rabbitMq.CreateSetupForBasicRetry();
+                    break;
+                case "AdvancedRetry":
+                case "AR":
+                    rabbitMq.CreateSetupForAdvancedRetry();
+                    break;
+                case "DeadLetterQueue":
+                case "DLQ":
+                    rabbitMq.CreateSetupForDeadLetterQueue();
+                    break;
+                case "RoutingFailure":
+                case "RF":
+                    rabbitMq.CreateSetupForRoutingFailure();
+                    break;
+                case "ScheduledDelivery":
+                case "SD":
+                    rabbitMq.CreateSetupForScheduledDelivery();
+                    break;
                 default:
-                    return String.Empty;
+                    rabbitMq.CreateSetupForOneWayMessaging();
+                    break;
             }
+
+            Console.WriteLine($"Setup done for {type} Exchange Pattern");
+
         }
 
-        private static byte[] SerializeMessage(Message messageObj, string format)
-        {
-            switch (format)
-            {
-                case "1":
-                case "JSON":
-                    var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(messageObj);
-                    return Encoding.Default.GetBytes(jsonString);
-                case "2":
-                case "XML":
-                    var xmlMessageStream = new MemoryStream();
-                    var xmlSerializer = new XmlSerializer(messageObj.GetType());
-                    xmlSerializer.Serialize(xmlMessageStream, messageObj);
-                    xmlMessageStream.Flush();
-                    xmlMessageStream.Seek(0, SeekOrigin.Begin);
-                    return xmlMessageStream.GetBuffer();
-                case "3":
-                case "Binary":
-                    var binaryMessageStream = new MemoryStream();
-                    var binarySerializer = new BinaryFormatter();
-                    binarySerializer.Serialize(binaryMessageStream, messageObj);
-                    binaryMessageStream.Flush();
-                    binaryMessageStream.Seek(0, SeekOrigin.Begin);
-                    return binaryMessageStream.GetBuffer();
-                default:
-                    return null;
-            }
-        }
+
 
     }
+
 }
