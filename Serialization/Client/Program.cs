f6f4c0dbe8252a351472459c5ebd5fa614379771
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 11:54:36 +0530

    Update Program.cs

--- Serialization/Client/Program.cs
+++ Serialization/Client/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Contract;
 
 namespace Client
 {
@@ -10,6 +11,32 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ Message Sender");
+            Console.WriteLine();
+
+            var messageCount = 1;
+            var sender = new RabbitSender();
+
+            Console.WriteLine("Press enter key to send a message");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Q) break;
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    var messageObj = new Message { MyMessage = $"Message: {messageCount}" };
+                    Console.WriteLine("Sending - {0}", messageObj.MyMessage);
+
+                    var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(messageObj);
+                    var messageBuffer = Encoding.Default.GetBytes(jsonString);
+                    sender.Send(messageBuffer);
+                    messageCount++;
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
