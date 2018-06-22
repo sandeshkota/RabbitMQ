01809e6880f18dcafad52d392685c8e6954e8dd3
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 12:08:58 +0530

    Update Program.cs

--- LargeMessage/Buffered/Client/Program.cs
+++ LargeMessage/Buffered/Client/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,38 @@ namespace Client
 {
     class Program
     {
+        private const string InputFile = "BigFile.txt";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ Message Sender");
+            Console.WriteLine();
+
+            var messageCount = 1;
+            var sender = new RabbitSender();
+
+            Console.WriteLine("Press enter key to send a message");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Q) break;
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    // Read File
+                    Console.WriteLine($"Reading file - {InputFile}");
+                    byte[] messageBuffer = File.ReadAllBytes(InputFile);
+
+                    // Send Message
+                    Console.WriteLine($"Sending large message - {messageBuffer.Length}");
+                    sender.Send(messageBuffer);
+                    messageCount++;
+                    Console.WriteLine("Message sent");
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
