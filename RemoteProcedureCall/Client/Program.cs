41c00cc6eab4896e39a76b6d4b9c27dca915eef4
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 12:01:54 +0530

    Update Program.cs

--- RemoteProcedureCall/Client/Program.cs
+++ RemoteProcedureCall/Client/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,30 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ Message Sender");
+            Console.WriteLine();
+
+            var messageCount = 1;
+            var sender = new RabbitSender();
+
+            Console.WriteLine("Press enter key to send a message");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Q) break;
+
+                if (key.Key != ConsoleKey.Enter) continue;
+                var message = $"Message: {messageCount}";
+                Console.WriteLine("Sending - {0}", message);
+
+                var response = sender.Send(message, new TimeSpan(0,0,3,0));
+
+                Console.WriteLine("Response - {0}", response);
+                messageCount++;
+            }
+
+            Console.ReadLine();
         }
     }
 }
