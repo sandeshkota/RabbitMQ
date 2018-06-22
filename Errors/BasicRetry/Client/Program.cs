ebe66233e09183d0bebf43d9b25d0fc678c6249f
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 12:20:13 +0530

    Update Program.cs

--- Errors/BasicRetry/Client/Program.cs
+++ Errors/BasicRetry/Client/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Client
 {
@@ -10,6 +11,23 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ Message Sender");
+            Console.WriteLine();
+
+            var sender = new RabbitSender();
+
+            Console.WriteLine("Press enter key to send a message");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input.ToUpper() == "Q") break;
+                Console.WriteLine($"Sending - {input}");
+                sender.Send(input);
+            }
+
+            Console.ReadLine();
         }
     }
 }
