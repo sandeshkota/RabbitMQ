0efe7123763bb85582a54874b264fd0025a45153
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 11:56:32 +0530

    Update Program.cs

--- ScheduledDelivery/Client/Program.cs
+++ ScheduledDelivery/Client/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,27 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ Message Sender");
+            Console.WriteLine();
+
+            var messageCount = 1;
+            var sender = new RabbitSender();
+
+            Console.WriteLine("Enter the time delay & Press enter key to send a message");
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                int delay;
+
+                if (input.ToUpper() == "Q") break;
+                if (!int.TryParse(input, out delay)) continue;
+
+                var message = $"Message: {messageCount}";
+                Console.WriteLine("Sending - {0}", message);
+                sender.Send(message, input);
+                messageCount++;
+            }
         }
     }
 }
