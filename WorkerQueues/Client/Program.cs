d06133b67bc8fd95200886168ceea1037740aa89
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 11:47:37 +0530

    Update Program.cs

--- WorkerQueues/Client/Program.cs
+++ WorkerQueues/Client/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,29 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ Message Sender");
+            Console.WriteLine();
+
+            var messageCount = 1;
+            var sender = new RabbitSender();
+
+            Console.WriteLine("Press enter key to send a message");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Q) break;
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    var message = $"Message: {messageCount}";
+                    Console.WriteLine("Sending - {0}", message);
+                    sender.Send(message);
+                    messageCount++;
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
