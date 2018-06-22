b5d4a2011c5f73b1d6f329be82bc5ed613b4150e
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 11:53:47 +0530

    Update Program.cs

--- Topics/Server3/Program.cs
+++ Topics/Server3/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Server3
+namespace Server
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ queue processor");
+            Console.WriteLine();
+
+            Console.WriteLine("ROUTING KEY: *.medium.*");
+            Console.WriteLine("ROUTING KEY: corporate.#");
+            Console.WriteLine();
+
+            var queueProcessor = new RabbitConsumer() { Enabled = true };
+            queueProcessor.Start();
+
+            Console.ReadLine();
         }
     }
+
 }
