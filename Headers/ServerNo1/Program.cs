3a40c2e89e681e2054dd268fc7ac5c0484ad7f26
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 12:16:45 +0530

    Update Program.cs

--- Headers/ServerNo1/Program.cs
+++ Headers/ServerNo1/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ServerNo1
+namespace Server
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ queue processor :: Server No 1");
+            Console.WriteLine("Headers: material: wood, customertype: b2b");
+            Console.WriteLine();
+
+
+            var queueProcessor = new RabbitConsumer() { Enabled = true };
+            queueProcessor.Start();
+
+            Console.ReadLine();
         }
     }
+
 }
