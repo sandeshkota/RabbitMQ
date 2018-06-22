e66e28ac6802b8aca575b8da3b591b92684a87be
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 12:21:45 +0530

    Update Program.cs

--- Errors/DeadLetterQueue/DeadLetterQueueProcessor/Program.cs
+++ Errors/DeadLetterQueue/DeadLetterQueueProcessor/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DeadLetterQueueProcessor;
 
-namespace DeadLetterQueueProcessor
+namespace Server
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ queue processor");
+            Console.WriteLine();
+
+            var queueProcessor = new DeadLetterConsumer() { Enabled = true };
+            queueProcessor.Start();
+
+            Console.ReadLine();
         }
     }
+
 }
