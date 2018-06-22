27acd92ec71cd2bebe9baecc6ab617135d108a71
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 12:08:00 +0530

    Create Message.cs

---
+++ MessageFormat/Contract/Message.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    [Serializable]
+    public class Message
+    {
+        public string MyMessage { get; set; }
+    }
+}
