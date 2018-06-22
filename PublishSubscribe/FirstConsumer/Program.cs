e8f6c986a3dce27108864c465e280b09e98aa59b
Sandesh Kota <sandeshjkota@gmail.com>
2018-06-22 12:03:33 +0530

    Update Program.cs

--- PublishSubscribe/FirstConsumer/Program.cs
+++ PublishSubscribe/FirstConsumer/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstConsumer
+namespace Server
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ queue processor");
+            Console.WriteLine();
+
+            var queueProcessor = new RabbitConsumer() { Enabled = true };
+            queueProcessor.Start();
+
+            Console.ReadLine();
         }
     }
+
 }
